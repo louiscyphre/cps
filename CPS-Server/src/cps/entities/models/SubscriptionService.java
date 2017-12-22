3356b27b29a9513d7565e75303375fc1149507ba
Tegra Morgan <tegra.morgan.net@gmail.com>
2017-12-22 16:07:44 +0200

    Subscription edits

--- CPS-Server/src/cps/entities/models/SubscriptionService.java
+++ CPS-Server/src/cps/entities/models/SubscriptionService.java
@@ -1,8 +1,8 @@
 package cps.entities.models;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 // Database entity for monthly parking subscriptions - regular or full both stored in the same table .
 
 public class SubscriptionService extends Entity {
@@ -13,8 +13,12 @@ public class SubscriptionService extends Entity {
 	int customerID;
 	int carID;
 	int lotID; // if null then full, else regular
-	LocalDate startDate;
-	LocalDate endDate;
-	LocalTime endTime; // null for full subscription
+	Timestamp startDate;
+	Timestamp endDate;
+	Timestamp endTime; // null for full subscription
 	String email;
+
+	public static SubscriptionService buildFromQueryResult(ResultSet rs) throws SQLException {
+		return new SubscriptionService(rs.getInt(1),rs.getInt(2),rs.getInt(3),rs.getInt(4),rs.getInt(5),rs.getTimestamp(6),rs.getTimestamp(7),rs.getTimestamp(8),rs.getString(9));}
+	
 }
