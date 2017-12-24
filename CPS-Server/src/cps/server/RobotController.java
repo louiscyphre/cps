9a6cb4d37dee00185a49e1f0545fd2cb59b5d917
Augustin-Louis Cauchy <cauchy@vm>
2017-12-24 17:42:23 +0200

    Backported new classes from dev-Cauchy-for-diagrams

---
+++ CPS-Server/src/cps/server/RobotController.java
@@ -0,0 +1,16 @@
+package cps.server;
+
+import java.util.Map;
+
+public class RobotController {
+	@SuppressWarnings("unused")
+	private Map<Integer, Robot> robots;
+	
+	public void insertCar(int lotID, String carID, int i, int j, int k) {
+		
+	}
+	
+	public void retrieveCar(int lotID, String carID, int i, int j, int k) {
+		
+	}
+}
