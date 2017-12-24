fb5fb3344ad74b6f03a042e4d805fa2c7e4755ac
Augustin-Louis Cauchy <cauchy@vm>
2017-12-24 17:40:11 +0200

    Backported the new classes from dev-Cauchy-for-diagrams

--- CPS-Server/src/cps/api/action/InitLotAction.java
+++ CPS-Server/src/cps/api/action/InitLotAction.java
@@ -1,10 +1,10 @@
 package cps.api.action;
 
-public class InitParkingLot extends LotAction {
+public class InitLotAction extends LotAction {
 	private static final long serialVersionUID = 1L;
 	private int size;
 
-	public InitParkingLot(int userID, int lotID, int size) {
+	public InitLotAction(int userID, int lotID, int size) {
 		super(userID, lotID);
 		this.size = size;
 	}
---
+++ CPS-Server/src/cps/api/action/ReserveParkingSlotsAction.java
@@ -0,0 +1,40 @@
+package cps.api.action;
+
+public class ReserveParkingSlotsAction extends LotAction {
+	private static final long serialVersionUID = 1L;
+	private int locationI;
+	private int locationJ;
+	private int locationK;
+
+	public ReserveParkingSlotsAction(int userID, int lotID, int locationI, int locationJ, int locationK) {
+		super(userID, lotID);
+		this.locationI = locationI;
+		this.locationJ = locationJ;
+		this.locationK = locationK;
+	}
+
+	public int getLocationI() {
+		return locationI;
+	}
+
+	public void setLocationI(int locationI) {
+		this.locationI = locationI;
+	}
+
+	public int getLocationJ() {
+		return locationJ;
+	}
+
+	public void setLocationJ(int locationJ) {
+		this.locationJ = locationJ;
+	}
+
+	public int getLocationK() {
+		return locationK;
+	}
+
+	public void setLocationK(int locationK) {
+		this.locationK = locationK;
+	}
+
+}
