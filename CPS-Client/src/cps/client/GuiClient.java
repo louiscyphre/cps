c4f72108554a6b8d049ad859333b5c9803aa0110
louiscyphre <dev.cccp@gmail.com>
2017-12-26 16:02:01 +0200

    Created GuiClient class.

---
+++ CPS-Client/src/cps/client/GuiClient.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package cps.client;
+
+/**
+ * @author student
+ *
+ */
+public class GuiClient implements ClientUI {
+
+  /**
+   * 
+   */
+  public GuiClient() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /* (non-Javadoc)
+   * @see cps.client.ClientUI#display(java.lang.Object)
+   */
+  @Override
+  public void display(Object message) {
+    // TODO Auto-generated method stub
+
+  }
+
+  /* (non-Javadoc)
+   * @see cps.client.ClientUI#displayError(java.lang.Object)
+   */
+  @Override
+  public void displayError(Object message) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
