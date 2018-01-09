7fdeaa9972dfcb6d98ce84a922a88777544efbd3
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-01-09 23:17:51 +0200

    Preparing tests
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

---
+++ CPS-Server/src/cps/server/testing/tests/TegraTests.java
@@ -0,0 +1,59 @@
+package cps.server.testing.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TegraTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLotController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertCar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetrieveCar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandleInitLotAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandleUpdatePricesAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHandleSetFullLotAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRequestController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDatabaseController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetServerController() {
+		fail("Not yet implemented");
+	}
+
+}
