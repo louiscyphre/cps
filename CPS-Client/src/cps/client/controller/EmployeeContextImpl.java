edbfffa956defd1a9cb4df2b15bb15cfcb784efb
firluk <firluk@gmail.com>
2018-01-10 22:00:23 +0200

    Defined Context

---
+++ CPS-Client/src/cps/client/controller/EmployeeContextImpl.java
@@ -0,0 +1,39 @@
+package cps.client.controller;
+
+public class EmployeeContextImpl implements EmployeeContext {
+
+  private int id;
+  private String username;
+  private int accessLevel;
+  
+  @Override
+  public int getEmployeeId() {
+    return this.id;
+  }
+
+  @Override
+  public void setEmployeeId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getEmployeeUsername() {
+    return this.username;
+  }
+
+  @Override
+  public void setEmployeeUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public int getEmployeeAccessLevel() {
+    return this.accessLevel;
+  }
+
+  @Override
+  public void setEmployeeAccessLevel(int accessLevel) {
+    this.accessLevel = accessLevel;
+  }
+
+}
