a23880c6e00b59cb8f365fec95530f02e79c279f
louiscyphre <dev.cccp@gmail.com>
2018-01-10 02:24:36 +0200

    Fix for main scene, added scene for reserve parking

---
+++ CPS-Client/src/cps/client/controller/NullableTimestamp.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package cps.client.controller;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+/**
+ * Created on: 2018-01-09 10:33:40 PM 
+ */
+public class NullableTimestamp extends Timestamp {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public NullableTimestamp() {
+      super(0L);
+  }
+  public NullableTimestamp(long value) {
+      super(value);
+  }
+
+  @Override
+  public String toString() {
+      return this.getTime() > 0L ? super.toString() : "";
+  }
+
+  public static NullableTimestamp valueOf(LocalDateTime localDateTime) {
+      return new NullableTimestamp(Timestamp.valueOf(localDateTime).getTime());
+  }
+}
---
+++ CPS-Client/src/cps/client/controller/ReserveParkingController.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package cps.client.controller;
+
+//import java.time.LocalDate;
+//import java.util.Calendar;
+//import java.util.Locale;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.DatePicker;
+//import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+//import jfxtras.scene.control.CalendarPicker;
+
+/**
+ * Created on: 2018-01-09 8:26:06 PM 
+ */
+public class ReserveParkingController implements ViewController {
+
+
+  @FXML
+  private Label infoLabel;
+
+  @FXML
+  private DatePicker endDatePicker;
+
+  @FXML
+  private ProgressIndicator infoProgress;
+
+  @FXML
+  private DatePicker startDatePicker;
+
+  @FXML
+  private Font x1;
+
+  @FXML
+  private VBox infoBox;
+
+  @FXML
+  private Insets x2;
+
+  @FXML
+  private Insets x3;
+
+  @FXML
+  private Insets x4;
+
+  @FXML
+  private TextField carIDTextField;
+
+    @FXML
+    void handleBackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSubmitButton(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void handlePickStartDateButton(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void initialize() {
+      assert infoBox != null : "fx:id=\"infoBox\" was not injected: check your FXML file 'LoginScene.fxml'.";
+      assert infoLabel != null : "fx:id=\"infoLabel\" was not injected: check your FXML file 'LoginScene.fxml'.";
+      assert infoProgress != null : "fx:id=\"infoProgress\" was not injected: check your FXML file 'LoginScene.fxml'.";
+
+      ControllersClientAdapter.registerCtrl(this,ControllerConstants.SceneCode.RESERVE_PARKING);
+
+      //startDatePicker = new DateTimePicker();
+      //startDatePicker.withCalendar(Calendar.getInstance());
+      //startDatePicker.withShowTime(Boolean.TRUE);
+      //startDatePicker.withLocale(Locale.ENGLISH);
+      //startDatePicker.calendarProperty().addListener(new ChangeListener<Calendar>() {
+
+      //    @Override
+       //   public void changed(ObservableValue<? extends Calendar> ov, Calendar t, Calendar t1) {
+       //       System.out.println("Selected date: "+t1.getTime().toString());
+       //   }
+     // });
+
+    }
+}
