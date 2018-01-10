a73655922985c54649c52fb953ea54cf141058ac
firluk <firluk@gmail.com>
2018-01-10 22:02:34 +0200

    Adapter and CRHandler minor changes

--- CPS-Client/src/cps/client/controller/responsehandler/CustomerResponseHandler.java
+++ CPS-Client/src/cps/client/controller/responsehandler/CustomerResponseHandler.java
@@ -3,6 +3,13 @@ package cps.client.controller.responsehandler;
 import cps.api.response.*;
 
 public interface CustomerResponseHandler {
+  
+  public void setCustomerId(int id);
+  public int getCustomerId();
+  
+  public void setCustomerEmail(String email);
+  public String getCustomerEmail();
+  
   public ServerResponse handle(CancelOnetimeParkingResponse response);
   public ServerResponse handle(ComplaintResponse response);
   public ServerResponse handle(FullSubscriptionResponse response);
