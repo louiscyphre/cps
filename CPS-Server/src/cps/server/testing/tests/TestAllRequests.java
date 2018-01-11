9ec17f3db4521a7b5a429b57ef104c46ce4d4ed3
Augustin-Louis Cauchy <cauchy@vm>
2018-01-11 22:11:29 +0200

    Fixed SubscriptionService::findByID

---
+++ CPS-Server/src/cps/server/testing/tests/TestAllRequests.java
@@ -0,0 +1,13 @@
+package cps.server.testing.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestCancelOnetimeParking.class, TestComplaint.class, TestFullSubscription.class,
+		TestIncidentalParking.class, TestListParkingLots.class, TestLogin.class, TestRegularSubscription.class,
+		TestReservedParking.class })
+public class TestAllRequests {
+
+}
