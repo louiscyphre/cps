a3550159d97cd2cc707b007c8fd6fbb5ea413b49
firluk <firluk@gmail.com>
2018-01-14 10:10:34 +0200

    ServiceLogin scene + contoller , css new class Medium Button

--- CPS-Client/src/cps/client/controller/service/ServiceLoginSceneController.java
+++ CPS-Client/src/cps/client/controller/service/ServiceLoginSceneController.java
@@ -4,9 +4,14 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import cps.api.action.ServiceLoginAction;
+import cps.client.controller.ControllerConstants;
+import cps.client.controller.ControllerConstants.InputVerification;
 import cps.client.controller.ControllerConstants.SceneCode;
 import cps.client.controller.ControllersClientAdapter;
 import cps.client.controller.ViewController;
+import cps.client.utils.FormatValidation;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ProgressIndicator;
@@ -16,6 +21,8 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class ServiceLoginSceneController implements ViewController {
+
+  private static final String DEFAULT_INFO_LABEL = "Welcome to Car Parking System!";
 
   @FXML // ResourceBundle that was given to the FXMLLoader
   private ResourceBundle resources;
@@ -38,14 +45,14 @@ public class ServiceLoginSceneController implements ViewController {
   @FXML // fx:id="infoBox"
   private VBox infoBox; // Value injected by FXMLLoader
 
-  @FXML
-  void handleCancelButton(ActionEvent event) {
-    ControllersClientAdapter.setStage(SceneCode.SERVICE_ACTION_MAIN_MENU);
-  }
+  private boolean processing = false;
 
   @FXML
-  void handleOkButton(ActionEvent event) {
-
+  void handleLogInButton(ActionEvent event) {
+    if(processing) {
+      return;
+    }
+    validateAndSend();
   }
 
   @FXML // This method is called by the FXMLLoader when initialization is
@@ -57,60 +64,119 @@ public class ServiceLoginSceneController implements ViewController {
     assert passwordTF != null : "fx:id=\"passwordTF\" was not injected: check your FXML file 'ServiceLoginScene.fxml'.";
     assert infoBox != null : "fx:id=\"infoBox\" was not injected: check your FXML file 'ServiceLoginScene.fxml'.";
     ControllersClientAdapter.registerCtrl(this, SceneCode.SERVICE_ACTION_LOGIN);
+    Platform.runLater(() -> infoBox.requestFocus()); // to unfocus the Text
+                                                     // Field
+  }
 
+  private void validateAndSend() {
+    String username = null;
+    try {
+      username = getUsername();
+    } catch (NumberFormatException e) {
+      displayError(InputVerification.MISSING_USERID.getMsg());
+      return;
+    }
+    String password = null;
+    try {
+      password = getPassword();
+    } catch (NumberFormatException e) {
+      displayError(InputVerification.MISSING_PASSWORD.getMsg());
+      return;
+    }
+    if (!FormatValidation.InputFormats.USERNAME.validate(username)) {
+      displayError(FormatValidation.InputFormats.USERNAME.errorMsg());
+      return;
+    }
+
+    ServiceLoginAction loginRequest = new ServiceLoginAction(username, password);
+    ControllersClientAdapter.getClient().sendRequest(loginRequest);
+  }
+
+  private String getPassword() {
+    return passwordTF.getText();
+  }
+
+  private String getUsername() {
+    return usernameTF.getText();
+  }
+
+  @FXML
+  void handleBackButton(ActionEvent event) {
+    ControllersClientAdapter.setStage(ControllerConstants.SceneCode.CUSTOMER_INITIAL_MENU);
   }
 
   @Override
   public void displayInfo(List<Text> formattedText) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void displayError(List<Text> formettedErrorMsg) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void turnProcessingStateOn() {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void turnProcessingStateOff() {
-    // TODO Auto-generated method stub
-
+    infoBox.getStyleClass().clear();
+    infoBox.getStyleClass().add("infoLabel");
+    infoLabel.getChildren().clear();
+    for (Text ft : formattedText) {
+      infoLabel.getChildren().add(ft);
+    }
   }
 
   @Override
   public void displayInfo(String simpleInfoMsg) {
-    // TODO Auto-generated method stub
+    infoBox.getStyleClass().clear();
+    infoBox.getStyleClass().add("infoLabel");
+    infoLabel.getChildren().clear();
+    infoLabel.getChildren().add(new Text(simpleInfoMsg));
+  }
 
+  @Override
+  public void displayError(List<Text> formettedErrorMsg) {
+    infoBox.getStyleClass().clear();
+    infoBox.getStyleClass().add("errorLabel");
+    infoLabel.getChildren().clear();
+    for (Text ft : formettedErrorMsg) {
+      infoLabel.getChildren().add(ft);
+    }
   }
 
   @Override
   public void displayError(String simpleErrorMsg) {
-    // TODO Auto-generated method stub
+    infoBox.getStyleClass().clear();
+    infoBox.getStyleClass().add("errorLabel");
+    infoLabel.getChildren().clear();
+    infoLabel.getChildren().add(new Text(simpleErrorMsg));
+  }
 
+  @Override
+  public void turnProcessingStateOn() {
+    infoProgress.visibleProperty().set(true);
+    Text text = new Text("Processing...");
+    infoLabel.getChildren().clear();
+    infoLabel.getChildren().add(text);
+    infoBox.getStyleClass().clear();
+    infoBox.getStyleClass().add("processingLabel");
+    processing = true;
+  }
+
+  @Override
+  public void turnProcessingStateOff() {
+    infoProgress.visibleProperty().set(false);
+    processing = false;
+    displayInfo(DEFAULT_INFO_LABEL);
   }
 
   @Override
   public void turnLoggedInStateOn() {
-    // TODO Auto-generated method stub
-
+    // view does not change
   }
 
   @Override
   public void turnLoggedInStateOff() {
-    // TODO Auto-generated method stub
-
+    // view does not change
   }
 
   @Override
   public void cleanCtrl() {
-    // TODO Auto-generated method stub
-
+    // info box clear
+    infoBox.getStyleClass().add("infoLabel");
+    infoProgress.visibleProperty().set(false);
+    infoLabel.getChildren().clear();
+    // input fields clear
+    usernameTF.clear();
+    passwordTF.clear();
   }
 }
