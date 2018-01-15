cea764f40fc37e71d685660f83f64b3189ecac2c
louiscyphre <dev.cccp@gmail.com>
2018-01-16 01:00:40 +0200

    Fixes to login and buy subscriptions flow. Now after login or buy user getting back automatically to main screen, when he logged in to the system. Minor fix to main screen view after login.

--- CPS-Client/src/cps/client/controller/customer/FullSubscriptionController.java
+++ CPS-Client/src/cps/client/controller/customer/FullSubscriptionController.java
@@ -65,7 +65,19 @@ public class FullSubscriptionController extends CustomerActionControllerBase {
     }
     validateAndSend();
   }
-  
+
+
+  @Override
+  public void turnLoggedInStateOn() {
+    super.turnLoggedInStateOn();
+    emailTextField.setVisible(false);
+  }
+
+  @Override
+  public void turnLoggedInStateOff() {
+    super.turnLoggedInStateOff();
+    emailTextField.setVisible(true);
+  }
 
   private void validateAndSend() {
     // validation in same order as order in the form
