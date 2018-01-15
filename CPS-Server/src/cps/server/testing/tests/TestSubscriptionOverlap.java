8e9ded30cc852cfdbc20a77dce897247eb8dc38c
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-01-15 19:14:46 +0200

    Squashed bugs, finished some Overlap Tests
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- CPS-Server/src/cps/server/testing/tests/TestSubscriptionOverlap.java
+++ CPS-Server/src/cps/server/testing/tests/TestSubscriptionOverlap.java
@@ -12,40 +12,53 @@ import org.junit.Test;
 
 import com.sun.swing.internal.plaf.basic.resources.basic_de;
 
+import cps.api.request.FullSubscriptionRequest;
+import cps.api.request.SubscriptionRequest;
+import cps.api.response.FullSubscriptionResponse;
+import cps.api.response.ResponseHandler;
+import cps.api.response.ServerResponse;
 import cps.common.Constants;
+import cps.entities.models.Customer;
 import cps.entities.models.SubscriptionService;
 import cps.server.ServerConfig;
 import cps.server.ServerController;
 import cps.server.ServerException;
 import cps.server.controllers.DatabaseController;
 import cps.server.controllers.SubscriptionController;
+import cps.server.session.CustomerSession;
 import cps.server.session.SessionHolder;
 
 public class TestSubscriptionOverlap {
 
-	ServerController server;
-	DatabaseController db;
-	SessionHolder context = new SessionHolder();
+  ServerController   server;
+  DatabaseController db;
+  SessionHolder      context = new SessionHolder();
 
-	@Before
-	public void setUp() throws Exception {
-		this.server = new ServerController(ServerConfig.testing());
-		this.db = server.getDatabaseController();
-		db.truncateTables();
-	}
+  @Before
+  public void setUp() throws Exception {
+    this.server = new ServerController(ServerConfig.testing());
+    this.db = server.getDatabaseController();
+    db.truncateTables();
+  }
 
-	@Test
-	public void testHandleSubscriptionRequestCustomerSessionSubscriptionResponseLocalDateLocalDateLocalTime() throws ServerException {
-		SubscriptionController subc = server.getSubscriptionController();
-		LocalDate starttime = LocalDate.now();
-		LocalDate endtime = LocalDate.now().plusDays(24);
-		LocalTime dailyexittime = LocalTime.of(18, 0, 0);
-		db.performAction(conn -> {
-			SubscriptionService.create(conn, Constants.SUBSCRIPTION_TYPE_FULL, 1, "NO@email.com", "TL-12-12", 2,
-					starttime, endtime, dailyexittime);
+  @Test
+  public void testHandleSubscriptionRequestCustomerSessionSubscriptionResponseLocalDateLocalDateLocalTime()
+      throws ServerException {
+    SubscriptionController subc = server.getSubscriptionController();
+    LocalDate starttime = LocalDate.now();
+    LocalDate endtime = LocalDate.now().plusDays(24);
+    LocalTime dailyexittime = LocalTime.of(18, 0, 0);
+    db.performAction(conn -> {
+      SubscriptionService.create(conn, Constants.SUBSCRIPTION_TYPE_FULL, 1, "NO@email.com", "TL-12-12", 2, starttime,
+          endtime, dailyexittime);
+      FullSubscriptionRequest over = new FullSubscriptionRequest(1, "NO@email.com", "TL-12-12",
+          LocalDate.now().plusDays(1));
+      CustomerSession sess = new CustomerSession(new Customer(1, "NO@email.com", "123", 0f, 0f));
+      FullSubscriptionResponse serverResponse = new FullSubscriptionResponse();
+      ServerResponse response = subc.handle(over, sess, serverResponse, starttime, endtime, dailyexittime);
+      assertFalse(response.success());
+    });
 
-		});
-
-	}
+  }
 
 }
