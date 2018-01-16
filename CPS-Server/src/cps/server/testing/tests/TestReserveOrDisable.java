0b90a27e3e5c8dcec6b9dbf533f08b983832a3f3
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-01-16 16:47:11 +0200

    Working on test
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- CPS-Server/src/cps/server/testing/tests/TestReserveOrDisable.java
+++ CPS-Server/src/cps/server/testing/tests/TestReserveOrDisable.java
@@ -2,10 +2,20 @@ package cps.server.testing.tests;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.corba.se.impl.protocol.InfoOnlyServantCacheLocalCRDImpl;
+
+import cps.api.action.DisableParkingSlotsAction;
+import cps.api.action.ReserveParkingSlotsAction;
+import cps.api.request.ReservedParkingRequest;
+import cps.api.response.DisableParkingSlotsResponse;
+import cps.api.response.ServerResponse;
 import cps.entities.models.ParkingLot;
+import cps.entities.models.ParkingCell.ParkingCellVisitor;
 import cps.server.ServerConfig;
 import cps.server.ServerController;
 import cps.server.ServerException;
@@ -18,6 +28,7 @@ public class TestReserveOrDisable {
   ServerController   server;
   DatabaseController db;
   SessionHolder      context = new SessionHolder();
+
   @Before
   public void setUp() throws Exception {
     this.server = new ServerController(ServerConfig.testing());
@@ -27,19 +38,31 @@ public class TestReserveOrDisable {
 
   @Test
   public void testReserveOrDisable() throws ServerException {
-    ParkingLot lot=initParkingLot();
-    LotController lotcontroller=server.getLotController();
+    ParkingLot lot = initParkingLot();
+    String carId = "IL-12-12";
+    LocalDateTime exitTime = LocalDateTime.now();
+    LotController lotcontroller = server.getLotController();
     CarTransportationController transportationController = server.getTransportationController();
-    db.performQuery(query);
-    
-    transportationController.insertCar(
-    //lotcontroller.reserveOrDisable(session, serverResponse, lotID, i, j, k, visitor, successMessage)
-    fail("Not yet implemented");
+    db.performAction(conn -> {
+      transportationController.insertCar(conn, lot, carId, exitTime.plusMinutes(50));
+    });
+    DisableParkingSlotsAction disableAction = new DisableParkingSlotsAction(1, 1, 0, 2, 2);
+    ServerResponse res = server.handle(disableAction, context);
+    assertFalse(res.success());
+    disableAction = new DisableParkingSlotsAction(1, 1, 0, 2, 1);
+    res = server.handle(disableAction, context);
+    assertTrue(res.success());
+    ReserveParkingSlotsAction reserveAction = new ReserveParkingSlotsAction(1, 1, 0, 2, 2);
+    res = server.handle(reserveAction, context);
+    assertFalse(res.success());
+    reserveAction = new ReserveParkingSlotsAction(1, 1, 0, 2, 0);
+    res = server.handle(reserveAction, context);
+    assertTrue(res.success());
   }
 
   protected ParkingLot initParkingLot() throws ServerException {
     ParkingLot lot = db.performQuery(conn -> ParkingLot.create(conn, "Sesam 2", 4, 5, 3, "12.f.t43"));
     return lot;
   }
-  
+
 }
