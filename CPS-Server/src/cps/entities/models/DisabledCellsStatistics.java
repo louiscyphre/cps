23013c3d9fb487831348a238eb8c0c94b535f380
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-01-20 01:12:16 +0200

    some progress
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- CPS-Server/src/cps/entities/models/DisabledCellsStatistics.java
+++ CPS-Server/src/cps/entities/models/DisabledCellsStatistics.java
@@ -7,41 +7,38 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import cps.server.database.QueryBuilder;
+
 // TODO: Auto-generated Javadoc
-/**
- * The Class DisabledCellsStatistics.
- */
+/** The Class DisabledCellsStatistics. */
 @SuppressWarnings("unused")
 public class DisabledCellsStatistics {
 
   /** The lot id. */
-  private int           lotId;
-  
+  private int lotId;
+
   /** The date disabled. */
   private LocalDateTime dateDisabled;
-  
+
   /** The width. */
-  private int           width;
-  
+  private int width;
+
   /** The height. */
-  private int           height;
-  
+  private int height;
+
   /** The depth. */
-  private int           depth;
-  
+  private int depth;
+
   /** The date enabled. */
   private LocalDateTime dateEnabled;
 
-  /**
-   * Instantiates a new disabled cells statistics.
-   *
+  /** Instantiates a new disabled cells statistics.
    * @param _lotid the lotid
    * @param _dateDisabled the date disabled
    * @param _width the width
    * @param _height the height
    * @param _depth the depth
-   * @param _dateEnabled the date enabled
-   */
+   * @param _dateEnabled the date enabled */
   public DisabledCellsStatistics(int _lotid, LocalDateTime _dateDisabled, int _width, int _height, int _depth,
       LocalDateTime _dateEnabled) {
     this.lotId = _lotid;
@@ -52,12 +49,9 @@ public class DisabledCellsStatistics {
     this.dateEnabled = _dateEnabled;
   }
 
-  /**
-   * Instantiates a new disabled cells statistics.
-   *
+  /** Instantiates a new disabled cells statistics.
    * @param rs the rs
-   * @throws SQLException the SQL exception
-   */
+   * @throws SQLException the SQL exception */
   public DisabledCellsStatistics(ResultSet rs) throws SQLException {
     this.lotId = rs.getInt("lotid");
     this.dateDisabled = rs.getTimestamp("date_disabled").toLocalDateTime();
@@ -67,16 +61,13 @@ public class DisabledCellsStatistics {
     this.dateEnabled = rs.getTimestamp("date_enabled").toLocalDateTime();
   }
 
-  /**
-   * Creates the.
-   *
+  /** Creates the.
    * @param conn the conn
    * @param _lotid the lotid
    * @param _width the width
    * @param _height the height
    * @param _depth the depth
-   * @throws SQLException the SQL exception
-   */
+   * @throws SQLException the SQL exception */
   public static void create(Connection conn, int _lotid, int _width, int _height, int _depth) throws SQLException {
     PreparedStatement stmt = conn.prepareStatement("INSERT INTO disabled_slots_table VALUES(?,?,?,?,?,default)");
     int i = 1;
@@ -88,16 +79,13 @@ public class DisabledCellsStatistics {
     stmt.executeUpdate();
   }
 
-  /**
-   * Markfixed.
-   *
+  /** Markfixed.
    * @param conn the conn
    * @param _lotid the lotid
    * @param _width the width
    * @param _height the height
    * @param _depth the depth
-   * @throws SQLException the SQL exception
-   */
+   * @throws SQLException the SQL exception */
   public static void markfixed(Connection conn, int _lotid, int _width, int _height, int _depth) throws SQLException {
     PreparedStatement stmt = conn.prepareStatement(
         "UPDATE disabled_slots_table SET date_enabled=? WHERE lotid=? AND width=? AND height=? AND depth=? AND date_enabled is null");
@@ -110,16 +98,13 @@ public class DisabledCellsStatistics {
     stmt.executeUpdate();
   }
 
-  /**
-   * Count disabled cells.
-   *
+  /** Count disabled cells.
    * @param conn the conn
    * @param lotid the lotid
    * @param from the from
    * @param to the to
    * @return the int
-   * @throws SQLException the SQL exception
-   */
+   * @throws SQLException the SQL exception */
   public static int countDisabledCells(Connection conn, int lotid, LocalDateTime from, LocalDateTime to)
       throws SQLException {
     String helper = "";
@@ -142,6 +127,14 @@ public class DisabledCellsStatistics {
     stmt.setTimestamp(i++, Timestamp.valueOf(from));
     stmt.setTimestamp(i++, Timestamp.valueOf(to));
     return stmt.executeUpdate();
+  }
+
+  public static double countDIsableHours(Connection conn) {
+    double result = 0f;
+    String helper = "";
+    helper += "";
+
+    return result;
   }
 
 }
