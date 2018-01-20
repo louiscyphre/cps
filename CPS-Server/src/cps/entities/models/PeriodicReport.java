ed75832ea8cbd15dcdfa7eccabd4475afa72635c
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-01-20 18:32:03 +0200

    Periodic report completed
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- CPS-Server/src/cps/entities/models/PeriodicReport.java
+++ CPS-Server/src/cps/entities/models/PeriodicReport.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Collection;
 import cps.common.Utilities;;
 
@@ -41,21 +42,73 @@ public class PeriodicReport extends GenericReport<StatisticalData> {
     /* Adjust the start date to be the begining of the week */
     start = Utilities.findWeekStart(start);
 
-    int[][] mh = new int[2][weeks];
+    /* Zero Total, Average, and Median data */
+    for (int i = weeks; i < weeks + 3; i++) {
+      result[i].canceled = 0;
+      result[i].completed = 0;
+      result[i].hours = 0;
+    }
+    /* Set labels */
+    result[weeks].field = "Total";
+    result[weeks + 1].field = "Average";
+    result[weeks + 2].field = "Median";
 
-    /* Populate the week rows and prepare data to calculate Total, Average, and Median */
+    /* Create median helper */
+    double[][] mh = new double[3][weeks];
 
+    /* for every week of the month */
     for (int i = 0; i < weeks; i++) {
+
+      /* get data for one week */
       days = DailyStatistics.getSevenDaysForPeriodic(conn, start);
+
+      /* zero corresponding data */
       result[i].completed = 0;
       result[i].canceled = 0;
+
+      /* for every day in week */
       for (int j = 0; j < 7; j++) {
+
+        /* Populate the week rows and prepare data to calculate Total, Average, and Median */
         result[i].completed += days[j].getRealizedOrders();
         result[i].canceled += days[j].getCanceledOrders();
       }
-    }
 
-    return null;
+      /* Count total hours of disabled cells */
+      result[i].hours = DisabledCellsStatistics.countDisableHours(conn, start, start.plusDays(7));
+
+      /* Set label to week number */
+      result[i].field = String.format("Week %d", i);
+
+      /* Add this week's data to tolal */
+      result[weeks].completed += result[i].completed;
+      result[weeks].canceled += result[i].canceled;
+      result[weeks].hours += result[i].hours;
+
+      /* insert data to median helper */
+      mh[0][i] = result[i].completed;
+      mh[1][i] = result[i].canceled;
+      mh[2][i] = result[i].hours;
+    }
+    /* Calculate averages */
+    result[weeks + 1].completed = result[weeks].completed / weeks;
+    result[weeks + 1].canceled = result[weeks].canceled / weeks;
+    result[weeks + 1].hours = result[weeks].hours / weeks;
+
+    /* Calculate median */
+
+    Arrays.sort(mh[0]);
+    Arrays.sort(mh[1]);
+    Arrays.sort(mh[2]);
+
+    /* Take the middle as the Median */
+    /* For calendarical reasons it is always ok to take the third week as the median */
+
+    result[weeks + 2].completed = mh[0][2];
+    result[weeks + 2].canceled = mh[1][2];
+    result[weeks + 2].hours = mh[2][2];
+
+    return result;
 
   }
 }
