48bfc8505ceb9f2b19680ceb0cd0fbcfb5b8b2bc
louiscyphre <dev.cccp@gmail.com>
2018-01-22 04:00:06 +0200

    Complaints controller continued, left to write handler.

--- CPS-Client/src/cps/client/controller/service/ServiceStaticticsComplaints.java
+++ CPS-Client/src/cps/client/controller/service/ServiceStaticticsComplaints.java
@@ -1,24 +1,21 @@
 package cps.client.controller.service;
 
 import cps.client.controller.ControllerConstants.SceneCode;
-import cps.client.utils.FormatValidation.InputFormats;
+import cps.common.Constants;
 import cps.entities.models.ParkingLot;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
-import cps.api.action.ListComplaintsAction;
+import cps.api.action.GetQuarterlyReportAction;
 import cps.api.request.ListParkingLotsRequest;
-import cps.api.request.RegularSubscriptionRequest;
-import cps.api.response.ListComplaintsResponse;
 import cps.api.response.ListParkingLotsResponse;
+import cps.api.response.QuarterlyReportResponse;
 import cps.api.response.ServerResponse;
-import cps.client.controller.ControllerConstants;
 import cps.client.controller.ControllersClientAdapter;
 import cps.client.controller.ParkingLotsController;
 import javafx.collections.FXCollections;
@@ -55,7 +52,11 @@ public class ServiceStaticticsComplaints extends ServiceActionControllerBase imp
 
   /** Start date DatePicker */
   @FXML
-  private DatePicker datePicker;
+  private DatePicker startDatePicker;
+  
+  /** End date DatePicker */
+  @FXML
+  private DatePicker endDatePicker;
 
   /** Parking lots list ComboBox */
   @FXML // fx:id="parkingLotsList"
@@ -66,10 +67,21 @@ public class ServiceStaticticsComplaints extends ServiceActionControllerBase imp
 
   /** @param event */
   @FXML
-  void handleDateChoice(ActionEvent event) {
+  void handleStartDateChoice(ActionEvent event) {
     if (processing) {
       return;
     }
+    endDatePicker.setDisable(false);
+  }
+
+  /** @param event */
+  @FXML
+  void handleEndDateChoice(ActionEvent event) {
+    if (processing) {
+      return;
+    }
+    parkingLotsList.setDisable(false);
+    
     if (!parkingLotsList.getItems().isEmpty()) {
       parkingLotsList.getItems().clear();
       parkingLotsMap.clear();
@@ -78,7 +90,7 @@ public class ServiceStaticticsComplaints extends ServiceActionControllerBase imp
     turnProcessingStateOn();
     sendRequest(new ListParkingLotsRequest());
   }
-
+  
   /** @param event */
   @FXML
   void handleLotChoice(ActionEvent event) {
@@ -92,22 +104,47 @@ public class ServiceStaticticsComplaints extends ServiceActionControllerBase imp
   /** Validates that the fields and Sends API request to the server. */
   void validateAndSend() {
     // validation in same order as order in the form
-
+    LocalDate reportStartDate = getReportStartDate();
+    if (reportStartDate == null) {
+      displayError("Start date invalid");
+      return;
+    }
+    LocalDate reportEndDate = getReportEndDate();
+    if (reportEndDate == null) {
+      displayError("End date invalid");
+      return;
+    }
+    if (reportStartDate.compareTo(reportEndDate) > 0) {
+      displayError("Start date must be today, or future date");
+      return;
+    }
     int lotID = ControllersClientAdapter.getEmployeeContext().getChosenLotID();
-    LocalDate wantedDate = getWantedDate();
-
-    ListComplaintsAction request = new ListComplaintsAction();// FIXME//TODO Constructor
+    int userID = ControllersClientAdapter.getEmployeeContext().getCompanyPerson().getId();
+    GetQuarterlyReportAction request = new GetQuarterlyReportAction(userID, Constants.REPORT_TYPE_QUARTERLY, reportEndDate, reportEndDate, lotID);
     turnProcessingStateOn();
     ControllersClientAdapter.getClient().sendRequest(request);
   }
 
-  /** @return planned start date or null if empty */
-  private LocalDate getWantedDate() {
-    if (datePicker.getValue() == null) {
+  /**
+   * @return report start date or null if empty
+   */
+  private LocalDate getReportStartDate() {
+    return getDate(startDatePicker);
+  }
+  
+  /**
+   * @return report end date or null if empty
+   */
+  private LocalDate getReportEndDate() {
+    return getDate(endDatePicker);
+  }
+  
+  private LocalDate getDate(DatePicker picker) {
+    if (picker.getValue() == null) {
       return null;
     }
     try {
-      return datePicker.getValue();
+      return picker.getValue();
     } catch (DateTimeException e) {
       return null;
     }
@@ -136,13 +173,10 @@ public class ServiceStaticticsComplaints extends ServiceActionControllerBase imp
     }
     parkingLotsList.setDisable(true);
     parkingLotsMap.clear();
-    // Get the list of parking lots
-    turnProcessingStateOn();
-    sendRequest(new ListParkingLotsRequest());
   }
 
   @Override
-  public ServerResponse handle(ListComplaintsResponse response) {
+  public ServerResponse handle(QuarterlyReportResponse response) {
     // TODO Auto-generated method stub
     return super.handle(response);
   }
