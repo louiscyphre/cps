87f854f30940c606647fa1a11ae1f2eb5cff8824
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-05 14:51:43 +0200

    Weekly report generation
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- CPS-Server/src/cps/server/background/Reminder.java
+++ CPS-Server/src/cps/server/background/Reminder.java
@@ -4,11 +4,18 @@
 package cps.server.background;
 
 import java.sql.Connection;
+import java.time.DayOfWeek;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 import cps.entities.models.OnetimeService;
+import cps.entities.models.ParkingLot;
 import cps.entities.models.SubscriptionService;
+import cps.entities.models.WeeklyStatistics;
 import cps.server.ServerConfig;
 import cps.server.ServerException;
 import cps.server.TimeProvider;
@@ -17,14 +24,13 @@ import cps.server.database.DatabaseController;
 import static cps.common.Utilities.debugPrint;
 import static cps.common.Utilities.debugPrintln;
 
-/**
- * Runs in the background to remind users about important events.
+/** Runs in the background to remind users about important events.
  * 1. Ask users who are late to their reserved parking whether they are still interested in the reservation.
- * 2. Remind users who purchased a monthly subscription, that there is 1 week left before their subscription runs out.
- */
+ * 2. Remind users who purchased a monthly subscription, that there is 1 week left before their subscription runs out. */
 public class Reminder extends Thread {
-  DatabaseController db = null;
-  private TimeProvider clock;
+  DatabaseController         db = null;
+  private TimeProvider       clock;
+  private HashSet<LocalDate> checkedWeeks;
 
   /** Number of milliseconds to wait until the next check. */
   private static final long INTERVAL = 60000; // Once in 1 minute
@@ -35,10 +41,8 @@ public class Reminder extends Thread {
     this.clock = clock;
   }
 
-  /*
-   * (non-Javadoc)
-   * @see java.lang.Thread#run()
-   */
+  /* (non-Javadoc)
+   * @see java.lang.Thread#run() */
   @Override
   public void run() {
 
@@ -46,27 +50,26 @@ public class Reminder extends Thread {
       while (true) {
         Thread.sleep(INTERVAL);
 
-        /*
-         * This function will send messages to users that are late by at least
-         * one second
-         */
+        /* This function will send messages to users that are late by at least
+         * one second */
         warnLateCustomers();
 
-        /*
-         * This function will find users that are late more than 30 minutes and
+        /* This function will find users that are late more than 30 minutes and
          * have not been authorized to do that and will cancel their one time
-         * order
-         */
+         * order */
         cancelLateReservations();
 
-
-        /*
-         * This function will find users that purchased a monthly subscription
+        /* This function will find users that purchased a monthly subscription
          * and have 1 week left until it expires,
-         * and will notify them to renew their subscription.
-         */
+         * and will notify them to renew their subscription. */
         warnSubscriptionOwners();
 
+        /* This function will generate weekly reports */
+
+        if (clock.now().getDayOfWeek() == DayOfWeek.SUNDAY && checkedWeeks.contains(clock.now().toLocalDate())) {
+          generateLastWeek();
+          checkedWeeks.add(clock.now().toLocalDate());
+        }
       }
 
     } catch (InterruptedException e) {
@@ -76,11 +79,22 @@ public class Reminder extends Thread {
     }
   }
 
-  /**
-   * Find and warn customers who reserved a parking and did not show up yet.
-   * 
-   * @throws ServerException on error
-   */
+  /** Generate last week weekly report for each parking lot and populate it into the database.
+   * @throws ServerException the server exception */
+  private void generateLastWeek() throws ServerException {
+    db.performAction(conn -> {
+      debugPrint("Updating last week weekly report ");
+      LinkedList<ParkingLot> lots = (LinkedList<ParkingLot>) ParkingLot.findAll(conn);
+      for (ParkingLot lot : lots) {
+        WeeklyStatistics.createUpdateWeeklyReport(conn, clock.now().toLocalDate().minusDays(7), lot.getId());
+      }
+
+      debugPrintln("...updated %s lots weekly reports", lots.size());
+    });
+  }
+
+  /** Find and warn customers who reserved a parking and did not show up yet.
+   * @throws ServerException on error */
   private void warnLateCustomers() throws ServerException {
     db.performAction(conn -> {
       debugPrint("Searching late customers ");
@@ -97,12 +111,9 @@ public class Reminder extends Thread {
     });
   }
 
-  /**
-   * Find users that are late more than 30 minutes and cancel their reservation
+  /** Find users that are late more than 30 minutes and cancel their reservation
    * if they did not confirm that they are still interested.
-   * 
-   * @throws ServerException on error
-   */
+   * @throws ServerException on error */
   private void cancelLateReservations() throws ServerException {
     db.performAction(conn -> {
       debugPrint("Searching customers who forfeited their reservation");
@@ -122,9 +133,8 @@ public class Reminder extends Thread {
   private void sendWarning(Connection conn, OnetimeService late) {
     // TODO optional - send an email to email address
     // Email should be sent from this function when the client provides an SMTP server
-    System.out.printf(
-        "\nSending message to customer %d, to email %s.\n We are waiting for your car %s, at lot %s, at %s\n",
-        late.getCustomerID(), late.getEmail(), late.getCarID(), late.getLotID(), late.getPlannedStartTime());
+    System.out.printf("\nSending message to customer %d, to email %s.\n We are waiting for your car %s, at lot %s, at %s\n", late.getCustomerID(),
+        late.getEmail(), late.getCarID(), late.getLotID(), late.getPlannedStartTime());
   }
 
   private void warnSubscriptionOwners() throws ServerException {
@@ -142,6 +152,5 @@ public class Reminder extends Thread {
       debugPrintln("...sent %s subscription expiration warnings", items.size());
     });
 
-    
   }
 }
